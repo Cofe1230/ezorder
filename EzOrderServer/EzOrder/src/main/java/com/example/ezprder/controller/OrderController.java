4d26bb9d2865fce3ee6dc701be3bd0f3d53a94e2
Cofe1230 <winner1230@gmail.com>
2023-08-23 14:56:53 +0900

    토큰db저장

--- EzOrderServer/EzOrder/src/main/java/com/example/ezprder/controller/OrderController.java
+++ EzOrderServer/EzOrder/src/main/java/com/example/ezprder/controller/OrderController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.ezprder.model.Member;
 import com.example.ezprder.model.OrderInfo;
+import com.example.ezprder.service.MemberService;
 import com.example.ezprder.service.OrderService;
 
 import lombok.RequiredArgsConstructor;
@@ -21,17 +23,18 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class OrderController {
 	private final OrderService orderService;
+	private final MemberService memberService;
 	
 	//사용자(일반)가 가게에서 주문했을때 db로 주문 정보 입력
 	@PutMapping("insert")
 	public void save(@RequestBody OrderInfo orderInfo){
-		OrderInfo o = new OrderInfo(orderInfo);
+		orderInfo.setMember(memberService.findByMemberName(orderInfo.getMember().getMemberName()));
 		orderService.save(orderInfo);
 	}
 	//memberId로 주문정보 리스트
 	@GetMapping("list/{memberName}")
 	public List<OrderInfo> OrderInfoListByMemberId(@PathVariable String memberName){
-		
-		return orderService.findByMemberId(memberName);
+		Member member = memberService.findByMemberName(memberName);
+		return orderService.findByMemberId(member);
 	}
 }
